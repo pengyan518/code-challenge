8151bd1bce8f4bcfdd1f15aca6dd064b5e980213
peng.yan <peng.yan@ticketingbox.com>
2022-09-10 12:13:14 -0400

    curve improvement

--- curve/index.tsx
+++ curve/index.tsx
@@ -5,26 +5,37 @@ import {useMainContext} from '../contexts/MainContext'
 // @ts-ignore
 const Curve: React.FC = props => {
   const {hoursDetail} = useMainContext()
+  const itemWidth = 100
+  const itemHeight = 150
 
   const points = useRef([])
-  points.current = hoursDetail.hour
+  const curve = hoursDetail.hour
     .filter((hour: any, index: number) => index < 12)
-    .map((oneHour: {condition: any; temp_f: any; time_epoch: any; time: any}, index) => {
+    .map((oneHour: {condition: any; temp_f: any; time_epoch: any; time: any}, index: number) => {
       const {temp_f} = oneHour
-      return `${index*20},${temp_f}`
+      return `${index * itemWidth},${itemHeight - temp_f}`
     })
+  
+  // @ts-ignore
+  points.current = [`0,${itemHeight}`, ...curve, `${ (curve.length - 1)* itemWidth},${itemHeight}`]
   // @ts-ignore
   return (
     <>
       {/* @ts-ignore */}
-      <svg xmlns={config.xlmns} fill="currentColor" width={600} height={200} {...props}>
+      <svg
+        xmlns={config.xlmns}
+        fill="currentColor"
+        width={curve.length * itemWidth}
+        height={itemHeight}
+        viewBox={`0 0 ${curve.length * itemWidth} ${itemHeight}`}
+        {...props}>
         {/* @ts-ignore */}
         <g>
           {/* @ts-ignore */}
-          <polygon points={points.current} fill="red" />
-            {/* @ts-ignore */}
-        </g>
+          <polygon points={points.current.join(' ')} fill="red" />
           {/* @ts-ignore */}
+        </g>
+        {/* @ts-ignore */}
       </svg>
     </>
   )
