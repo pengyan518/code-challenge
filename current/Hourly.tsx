a42793293b7944730c6c6b2d332537386628fb0d
peng.yan@goshenyun.com <>
2022-09-10 22:27:13 -0400

    curve

--- current/Hourly.tsx
+++ current/Hourly.tsx
@@ -4,20 +4,21 @@ import Curve from '../curve'
 const Hourly = () => {
   const {hoursDetail} = useMainContext()
   return (
-    <div className="hours-grid-wrapper mx-auto mt-4">
+    <div className="hours-grid-wrapper mx-auto mt-4 relative">
       {hoursDetail.hour.length > 0 && (
         <>
           <Curve />
-          <div className="hours-grid grid">
+          <div className="hours-grid grid relative z-10">
             {hoursDetail.hour
               .filter((hour: any, index: number) => index < 12)
               .map((oneHour: {condition: any; temp_f: any; time_epoch: any; time: any}) => {
-                const {condition, temp_f, time_epoch, time} = oneHour
+                const {condition, temp_f, time_epoch, time, chance_of_rain} = oneHour
                 return (
-                  <div key={time_epoch}>
+                  <div key={time_epoch} className="text-center">
                     <div>{time.split(' ')[1]}</div>
-                    <div>{temp_f}°</div>
+                    <div className="hour-temp">{temp_f}°</div>
                     <img src={`https://${condition.icon}`} alt="" width={48} height={48} />
+                    <div>{chance_of_rain > 0 && (<span>{chance_of_rain}%</span>)}</div>
                   </div>
                 )
               })}
--- curve/index.tsx
+++ curve/index.tsx
@@ -9,18 +9,24 @@ const Curve: React.FC = props => {
   const itemHeight = 150
 
   const points = useRef([])
-  const curve = hoursDetail.hour
-    .filter((hour: any, index: number) => index < 12)
-    .map((oneHour: {condition: any; temp_f: any; time_epoch: any; time: any}, index: number) => {
-      const {temp_f} = oneHour
-      return `${index * itemWidth},${itemHeight - temp_f}`
-    })
-  
+  const baseHeight = 20
+  const hours = hoursDetail.hour.filter((hour: any, index: number) => index < 12)
+  const curve = hours.map((oneHour: {condition: any; temp_f: any; time_epoch: any; time: any}, index: number) => {
+    const {temp_f} = oneHour
+    return `${index * itemWidth + itemWidth / 2},${itemHeight - temp_f - baseHeight}`
+  })
+
   // @ts-ignore
-  points.current = [`0,${itemHeight}`, ...curve, `${ (curve.length - 1)* itemWidth},${itemHeight}`]
+  points.current = [
+    `0,${itemHeight}`,
+    `0,${itemHeight - hours[0].temp_f - baseHeight}`,
+    ...curve,
+    `${(curve.length) * itemWidth},${itemHeight - hours[curve.length - 1].temp_f - baseHeight}`,
+    `${(curve.length) * itemWidth},${itemHeight}`,
+  ]
   // @ts-ignore
   return (
-    <>
+    <div className="curve__wrapper absolute bottom-0 left-0 z-0">
       {/* @ts-ignore */}
       <svg
         xmlns={config.xlmns}
@@ -31,13 +37,14 @@ const Curve: React.FC = props => {
         {...props}>
         {/* @ts-ignore */}
         <g>
+          <polyline points={curve.join(' ')} stroke="#0a84ff" strokeWidth="2" fill="none"/>
           {/* @ts-ignore */}
-          <polygon points={points.current.join(' ')} fill="red" />
+          <polygon points={points.current.join(' ')} fill="#313542" />
           {/* @ts-ignore */}
         </g>
         {/* @ts-ignore */}
       </svg>
-    </>
+    </div>
   )
 }
 
